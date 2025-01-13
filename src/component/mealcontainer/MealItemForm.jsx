22572ea72ab786f75029b12a6863bd87ebe3d8d5
Odion <odionfiona@gmail.com>
2025-01-13 14:22:17 +0300

    modified:   src/component/mealcontainer/MealItemForm.jsx

--- src/component/mealcontainer/MealItemForm.jsx
+++ src/component/mealcontainer/MealItemForm.jsx
@@ -1,5 +1,5 @@
 import { useRef, useState } from 'react';
-import Input from "../UI/input";
+import Input from "../UI/Input";
 // import { useState } from 'react';
 
 const MealItemForm = (props) => {
